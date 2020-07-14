c0de3c951b5ee4b6c3700d0f8689ce80e705b7db
xaviermaster <xavier.velez3132@gmail.com>
2020-07-13 23:44:51 -0500

    Xavier Velez Zambrano

---
+++ core/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, HttpResponse
+
+html_base = """
+<h1>Mi web personal</h1>
+<ul>
+    <li><a href = "/">Portada</a></li>
+    <li><a href = "/about">Acerca de</a></li>
+    <li><a href = "/portfolio">Portafolio</a></li>
+    <li><a href = "/contact">Contacto</a></li>
+</ul>
+
+"""
+
+
+def home(request):
+    return render(request, 'core/home.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def portfolio(request):
+    return render(request, 'core/portfolio.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+# Create your views here.
